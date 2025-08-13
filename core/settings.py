c7e46f0482b0edec350dbecce8669583c69abd45
Ozodbek <ozodbekalijonov365@gmail.com>
2025-08-13 12:19:06 +0500

    first commit

--- core/settings.py
+++ core/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-tmmyo6v@^y5o%t8$vebe&)%hi*$7+ldkv!*+zj87a^mk37c$bk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
